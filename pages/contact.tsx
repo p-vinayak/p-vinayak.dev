c4a9ce940a1d5bdf5b6ba92d5b68b4b289d7a32b
Vinayak Prataap <prataap.vinayak@gmail.com>
2022-08-08 11:58:08 -0400

    Fix upgrade errors

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,27 +3,27 @@ import path from "path";
 import fs from "fs";
 import ContentPage from "../components/templates/ContentPage";
 import matter from "gray-matter";
-import marked from "marked"; // Will show an error until @types/marked v4 is released
+import { marked } from "marked";
 
 export const getStaticProps: GetStaticProps = async () => {
-	const file = path.join(process.cwd(), "/content/contact.md");
-	const fileContents = fs.readFileSync(file, "utf-8").toString();
-	const markdown = matter(fileContents);
-	const htmlContents = marked(markdown.content);
-	return { props: { htmlContents, data: markdown.data } };
+  const file = path.join(process.cwd(), "/content/contact.md");
+  const fileContents = fs.readFileSync(file, "utf-8").toString();
+  const markdown = matter(fileContents);
+  const htmlContents = marked(markdown.content);
+  return { props: { htmlContents, data: markdown.data } };
 };
 
 const Contact: NextPage = ({
-	htmlContents,
-	data,
+  htmlContents,
+  data,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-	return (
-		<ContentPage
-			title={data.title}
-			description={data.description}
-			htmlContent={htmlContents}
-		/>
-	);
+  return (
+    <ContentPage
+      title={data.title}
+      description={data.description}
+      htmlContent={htmlContents}
+    />
+  );
 };
 
 export default Contact;
